967b80e74fe7fbf10056cd3f1a809cd04761b306
Tadas <ttadas10@gmail.com>
2026-02-02 11:51:54 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tadasmil.github.io',
-  base: process.env.NODE_ENV === 'production' ? '/hidrografika' : undefined,
+  base: process.env.NODE_ENV === 'production' ? '/uzukampis' : undefined,
 });
